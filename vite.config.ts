e0904484dabb9ffe0e31a8b585f9debb457e9d77
nmanumr <nmanumr@gmail.com>
2024-05-26 14:07:33 +0500

    Add environment variable for Github Actions base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log(import.meta.env);
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: import.meta.env.GITHUB_ACTIONS_BASE ?? '/',
   plugins: [react()],
 })
