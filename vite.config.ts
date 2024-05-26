7a15c7b44327fb57f68324e81a158d3fae78aa73
nmanumr <nmanumr@gmail.com>
2024-05-26 14:13:49 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log(process.env);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS_BASE ?? '/',
